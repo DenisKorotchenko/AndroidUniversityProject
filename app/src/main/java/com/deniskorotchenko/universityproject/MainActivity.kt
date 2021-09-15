d8f16a5808e2dd7f5e9575d01730cd0bf0fe47bd
DenisKorotchenko <dzd1024ozd@gmail.com>
2021-09-15 14:10:43 +0300

    Add RecyclerView

--- app/src/main/java/com/deniskorotchenko/universityproject/MainActivity.kt
+++ app/src/main/java/com/deniskorotchenko/universityproject/MainActivity.kt
@@ -2,10 +2,30 @@ package com.deniskorotchenko.universityproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.usersRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        val adapter = UserAdapter()
+        recyclerView.adapter = adapter
+        adapter.userList = loadUsers()
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun loadUsers(): List<User> {
+        val list = mutableListOf<User>()
+        for (i in 0 until 15) {
+            list.add(User(
+                avatarUrl = "",
+                userName = "User Name $i",
+                groupName = "A$i"
+            ))
+        }
+        return list.toList()
     }
 }
---
+++ app/src/main/java/com/deniskorotchenko/universityproject/User.kt
@@ -0,0 +1,7 @@
+package com.deniskorotchenko.universityproject
+
+data class User(
+    val avatarUrl: String, // URL to image in format like this: "https//test.com/test.jpg"
+    val userName: String,
+    val groupName: String
+)
