a028bf42f8047c20298a788a8f29822e78c5dbd4
DenisKorotchenko <dzd1024ozd@gmail.com>
2021-12-05 11:59:28 +0300

    Add logout

--- app/src/main/java/com/deniskorotchenko/universityproject/ui/profile/ProfileFragmentViewModel.kt
+++ app/src/main/java/com/deniskorotchenko/universityproject/ui/profile/ProfileFragmentViewModel.kt
@@ -1,7 +1,40 @@
 package com.deniskorotchenko.universityproject.ui.profile
 
+import androidx.lifecycle.viewModelScope
+import com.deniskorotchenko.universityproject.interactor.AuthInteractor
 import com.deniskorotchenko.universityproject.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
-class ProfileFragmentViewModel : BaseViewModel() {
+@HiltViewModel
+class ProfileFragmentViewModel @Inject constructor(
+    private val authInteractor: AuthInteractor
+): BaseViewModel() {
+
+    private val _eventChannel = Channel<Event>(Channel.BUFFERED)
+
+    fun eventsFlow(): Flow<Event> {
+        return _eventChannel.receiveAsFlow()
+    }
+
+    fun logout() {
+        viewModelScope.launch {
+            try {
+                authInteractor.logout()
+            } catch (error: Throwable) {
+                Timber.e(error)
+                _eventChannel.send(Event.LogoutError(error))
+            }
+        }
+    }
+
+    sealed class Event {
+        data class LogoutError(val error: Throwable): Event()
+    }
 
 }
